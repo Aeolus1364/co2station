6778ef111f5e800753f47d3df71df52f826db93d
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-11-11 17:59:12 -0500

    Function to import values into python using paulvha's library

---
+++ main.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+def read():
+    result = subprocess.run(['./scd30', '-S'], stdout=subprocess.PIPE)
+    decoded = result.stdout.decode('utf-8')
+    data = decoded.split()[-8:]
+    co2 = data[0]
+    humidity = data[3]
+    temperature = data[6]
+    return co2, humidity, temperature
