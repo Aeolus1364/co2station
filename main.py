d974840ad6f156566d68b3fe4a766df3cf66b49d
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-11-11 18:20:43 -0500

    Read function more adapted to function changes

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import subprocess
 
 
 def read():
-    result = subprocess.run(['exec /home/pi/scd30_on_raspberry/scd30', '-S'], stdout=subprocess.PIPE)
+    result = subprocess.run(['/home/pi/scd30_on_raspberry/scd30', '-SB'], stdout=subprocess.PIPE)
     decoded = result.stdout.decode('utf-8')
-    data = decoded.split()[-8:]
-    co2 = data[0]
-    humidity = data[3]
-    temperature = data[6]
+    data = decoded.split()
+    co2, humidity, temperature = None, None, None
+    for n, w in enumerate(data):
+        m = w.lower()
+        if "co2" in m:
+            co2 = data[n + 1]
+        elif "humidity" in m:
+            humidity = data[n + 1]
+        elif "temperature" in m:
+            temperature = data[n + 1]
+
     return co2, humidity, temperature
