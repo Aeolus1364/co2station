7da20893d690e7d816e1d90e938beaee1bb6704b
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-11-11 18:03:24 -0500

    Function to import values into python using paulvha's library

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def read():
-    result = subprocess.run(['./scd30', '-S'], stdout=subprocess.PIPE)
+    result = subprocess.run(['exec /home/pi/scd30_on_raspberry/scd30', '-S'], stdout=subprocess.PIPE)
     decoded = result.stdout.decode('utf-8')
     data = decoded.split()[-8:]
     co2 = data[0]
