db0d1c356125d55d6d755b31549744202efb59a0
Aeolus1364 <thetesseractsshadow@gmail.com>
2019-01-21 21:48:24 -0500

    Twitter connection added

--- SCD30.py
+++ SCD30.py
@@ -1,5 +1,20 @@
 import subprocess
 import shlex
+import tweepy
+
+
+with open("keys", "r") as f:
+    text = f.read()
+    lines = text.splitlines()
+    consumer_key = lines[0].split("consumer_key=")[1]
+    consumer_secret = lines[1].split("consumer_secret=")[1]
+    access_token = lines[2].split("access_token=")[1]
+    access_token_secret = lines[3].split("access_token_secret=")[1]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
 
 def run_command(command):
@@ -11,6 +26,7 @@ def run_command(command):
         if output:
             text = output.strip().decode("utf-8").split()
             if text[0] == "CO2:":
+                api.update_status("Current CO2 level: " + co2)
                 co2 = text[1]
                 hum = text[4]
                 temp = text[7]
