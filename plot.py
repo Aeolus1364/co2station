9126b4e16409ba21c59a26d0743c8a80c2f4b2f1
Aeolus1364 <thetesseractsshadow@gmail.com>
2019-07-15 13:57:36 -0400

    changing data file name

--- plot.py
+++ plot.py
@@ -1,8 +1,12 @@
 from matplotlib import pyplot
 import pickle
 
-data = pickle.load(open("data1", "rb"))
+data = pickle.load(open("data2", "rb"))
 
-pyplot.plot(data[0])
+vals = data[0]
+dates = data[1]
+
+
+pyplot.plot_date(dates, vals, 'b-')
 # pyplot.plot(data1[1])
 pyplot.show()
